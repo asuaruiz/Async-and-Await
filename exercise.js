cefac0de357efba0da3e503e66c4c0c451008e4c
Andrea <asuaruiz@gmail.com>
2021-03-15 23:04:57 -0300

    changing nested callbacks into async function (promises)

--- exercise.js
+++ exercise.js
@@ -1,35 +1,54 @@
 
-getCustomer(1, (customer) => {
+// getCustomer(1, (customer) => {
+//   console.log('Customer: ', customer);
+//   if (customer.isGold) {
+//     getTopMovies((movies) => {
+//       console.log('Top movies: ', movies);
+//       sendEmail(customer.email, movies, () => {
+//         console.log('Email sent...')
+//       });
+//     });
+//   }
+// });
+
+async function sendMailToCustomer() { 
+  const customer = await getCustomer(1);
   console.log('Customer: ', customer);
-  if (customer.isGold) {
-    getTopMovies((movies) => {
-      console.log('Top movies: ', movies);
-      sendEmail(customer.email, movies, () => {
-        console.log('Email sent...')
+  if (customer.isGold){
+    const movies = await getTopMovies();
+    console.log('Top movies: ', movies); 
+    await sendEmail(customer.email, movies);
+    console.log('Email sent...');
+  } 
+};
+sendMailToCustomer();
+
+function getCustomer(id) {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve({ 
+        id: 1, 
+        name: 'Andrea Suarez', 
+        isGold: true, 
+        email: 'asuaruiz@gmail.com' 
       });
-    });
-  }
-});
-
-function getCustomer(id, callback) {
-  setTimeout(() => {
-    callback({ 
-      id: 1, 
-      name: 'Andrea Suarez', 
-      isGold: true, 
-      email: 'asuaruiz@gmail.com' 
-    });
-  }, 4000);  
+    }, 4000);  
+  })
+  
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(['movie1', 'movie2']);
-  }, 4000);
+function getTopMovies() {
+  return new Promise ((resolve,reject) => {
+    setTimeout(() => {
+      resolve(['movie1', 'movie2']);
+    }, 4000);
+  })
 }
 
-function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+function sendEmail(email, movies) {
+  return new Promise ((resolve,reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 4000);
+  })
 }
